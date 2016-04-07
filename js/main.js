ff1aff14f50f2d3176bb46f42c96293f9713a226
David Miranda <david@storylog.com>
2016-04-07 02:05:03 -0400

    Updating site

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var disqus_shortname = 'smashingthings';
+
+if (disqus_shortname && disqus_shortname !== 'example') {
+  (function () {
+      var s = document.createElement('script'); s.async = true;
+      s.type = 'text/javascript';
+      s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+      (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+  }());
+}
